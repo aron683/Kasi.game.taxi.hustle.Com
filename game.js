f4d2b105620f4396a172238b92b1718a06494bae
aron683 <Tiisetsoaron@gmail.com>
2025-06-06 18:29:41 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,15 +1,1 @@
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-// Basic intro
-ctx.fillStyle = "#fff";
-ctx.font = "26px sans-serif";
-ctx.fillText("🚕 Kasi Hustle: Taxi Wars", 50, 100);
-
-// Sound
-const bgMusic = new Audio("assets/music.mp3");
-bgMusic.loop = true;
-bgMusic.volume = 0.6;
-bgMusic.play();
+🚀 Final full game engine added
